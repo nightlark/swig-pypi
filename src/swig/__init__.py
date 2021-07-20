172a4c2a73a0b8ce8a35d2ac78a61310cd881243
Ryan Mast <mast.ryan@gmail.com>
2021-07-19 20:30:39 -0700

    Ensure a path separator is present before wildcard for globbing swig_lib

--- src/swig/__init__.py
+++ src/swig/__init__.py
@@ -12,7 +12,7 @@ SWIG_SHARE_DIR = os.path.join(DATA, 'share/swig')
 # should only be one swig_lib version in the data subfolder
 # make nicer if swig adds support for custom SWIG_LIB paths or relative swig_lib paths
 SWIG_LIB_ENV = os.environ
-SWIG_LIB_ENV["SWIG_LIB"] = glob.glob(SWIG_SHARE_DIR + '*')[0]
+SWIG_LIB_ENV["SWIG_LIB"] = glob.glob(SWIG_SHARE_DIR + os.path.sep + '*')[0]
 
 def _program(name, args):
     return subprocess.call([os.path.join(BIN_DIR, name)] + args, env=SWIG_LIB_ENV)
