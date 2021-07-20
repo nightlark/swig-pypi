a862bc5048c54f4e83d1ec4c85aa0f39fecbeb9a
Ryan Mast <mast.ryan@gmail.com>
2021-07-19 18:35:31 -0700

    Add versioneer config options

--- src/swig/__init__.py
+++ src/swig/__init__.py
@@ -2,13 +2,17 @@ import os
 import platform
 import subprocess
 import sys
+import glob
 
 DATA = os.path.join(os.path.dirname(__file__), 'data')
 
 BIN_DIR = os.path.join(DATA, 'bin')
 SWIG_SHARE_DIR = os.path.join(DATA, 'share/swig')
+
+# should only be one swig_lib version in the data subfolder
+# make nicer if swig adds support for custom SWIG_LIB paths or relative swig_lib paths
 SWIG_LIB_ENV = os.environ
-SWIG_LIB_ENV["SWIG_LIB"] = "blah"
+SWIG_LIB_ENV["SWIG_LIB"] = glob.glob(SWIG_SHARE_DIR + '*')[0]
 
 def _program(name, args):
     return subprocess.call([os.path.join(BIN_DIR, name)] + args, env=SWIG_LIB_ENV)
