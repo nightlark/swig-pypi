4a8056b433764565f7b21a865e9b37a62c9e0a13
Ryan Mast <nightlark@users.noreply.github.com>
2021-07-21 23:01:19 -0700

    Set pre-commit.ci options and do initial formatting (#6)
    * Set pre-commit.ci options
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -6,18 +6,21 @@ from skbuild import setup
 
 # may be needed in some cases for versioneer to get version correctly during builds
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-import versioneer # noqa: E402
+import versioneer  # noqa: E402
+
 
 class genericpy_bdist_wheel(_bdist_wheel):
     def finalize_options(self):
         _bdist_wheel.finalize_options(self)
         self.root_is_pure = False
+
     def get_tag(self):
         python, abi, plat = _bdist_wheel.get_tag(self)
-        python, abi = 'py2.py3', 'none'
+        python, abi = "py2.py3", "none"
         return python, abi, plat
 
-with open('README.md', 'r') as fp:
+
+with open("README.md", "r") as fp:
     readme = fp.read()
 
 cmdclass = {"bdist_wheel": genericpy_bdist_wheel}
@@ -25,37 +28,31 @@ for k, v in versioneer.get_cmdclass().items():
     cmdclass[k] = v
 
 setup(
-    name='swig',
+    name="swig",
     version=versioneer.get_version(),
     cmdclass=cmdclass,
-    package_dir={'': 'src'},
-    packages=['swig'],
-    cmake_install_dir='src/swig/data',
-    entry_points={
-        'console_scripts': [
-            'swig=swig:swig'
-        ]
-    },
-
-    url='http://www.swig.org/',
-    download_url='http://www.swig.org/download.html',
-    description='SWIG is a software development tool that connects '
-            'programs written in C and C++ with a variety of '
-            'high-level programming languages.',
+    package_dir={"": "src"},
+    packages=["swig"],
+    cmake_install_dir="src/swig/data",
+    entry_points={"console_scripts": ["swig=swig:swig"]},
+    url="http://www.swig.org/",
+    download_url="http://www.swig.org/download.html",
+    description="SWIG is a software development tool that connects "
+    "programs written in C and C++ with a variety of "
+    "high-level programming languages.",
     long_description=readme,
-    long_description_content_type='text/markdown',
-
+    long_description_content_type="text/markdown",
     classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'License :: Other/Proprietary License',
-        'Programming Language :: C',
-        'Programming Language :: C++',
-        'Programming Language :: Python',
-        'Operating System :: OS Independent',
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools'
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: Other/Proprietary License",
+        "Programming Language :: C",
+        "Programming Language :: C++",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
     ],
-    license='pick a license',
-    keywords='swig build c c++',
+    license="pick a license",
+    keywords="swig build c c++",
 )
